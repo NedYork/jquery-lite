96a47d46c3f13899f765cd00ee970c746d63f1bb
App Academy Student <appacademy@Apps-Mac-mini-72.local>
2016-01-14 11:12:24 -0500

    Add core function and create DOMNodeCollection.

--- jquery_lite.js
+++ jquery_lite.js
@@ -1,8 +1,26 @@
 (function () {
   window.$l = function (input) {
+    var nodeCollection;
     if (typeof input === "string") {
-      var nodeCollection = document.querySelectorAll(input);
+      nodeCollection = document.querySelectorAll(input);
       nodeCollection = [].slice.call(nodeCollection);
+    } else if (input instanceof HTMLElement) {
+      nodeCollection = [input];
+    }
+    return new DOMNodeCollection(nodeCollection);
+  };
+
+  var DOMNodeCollection = function (htmlElements) {
+    this.htmlElements = htmlElements;
+  };
+
+  DOMNodeCollection.prototype.html = function (content) {
+    if (typeof content === "undefined") {
+      return this.htmlElements[0].innerHTML;
+    } else {
+      for (var i = 0; i < this.htmlElements.length; i++) {
+        this.htmlElements[i].innerHTML = content;
+      }
     }
   };
 
